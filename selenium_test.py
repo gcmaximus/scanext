a2543b685a83ff85cdc4f303358342f172b89f29
Dei-Filius <hwdkong@hotmail.sg>
2023-05-06 22:26:43 +0800

    testing selenium

--- selenium_test.py
+++ selenium_test.py
@@ -10,9 +10,9 @@ options.add_experimental_option("detach", True)
 
 chrome_service = Service(executable_path=ChromeDriverManager().install())
 driver = Chrome(service=chrome_service, options=options)
-driver.get("https://is.gd/WVZvnI")
+driver.get("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x69\x73\x2e\x67\x64\x2f\x57\x56\x5a\x76\x6e\x49")
 ele = WebDriverWait(driver, 30).until(
-    EC.presence_of_all_elements_located((By.TAG_NAME, "video"))
+    EC.presence_of_all_elements_located((By.TAG_NAME, "\x76\x69\x64\x65\x6f"))
 )
 ActionChains(driver).send_keys(Keys.SPACE).perform()
 # driver.execute_script('document.getElementsByTagName("video")[0].play()')
