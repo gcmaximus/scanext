e8c35faf2a7bd519b0300c87bb1a5355339520af
Dei-Filius <hwdkong@hotmail.sg>
2023-05-07 00:45:29 +0800

    testing selenium

--- selenium_test.py
+++ selenium_test.py
@@ -5,14 +5,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 options = ChromeOptions()
 options.add_experimental_option("detach", True)
-
 chrome_service = Service(executable_path=ChromeDriverManager().install())
 driver = Chrome(service=chrome_service, options=options)
-driver.get("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x69\x73\x2e\x67\x64\x2f\x57\x56\x5a\x76\x6e\x49")
-ele = WebDriverWait(driver, 30).until(
-    EC.presence_of_all_elements_located((By.TAG_NAME, "\x76\x69\x64\x65\x6f"))
-)
-ActionChains(driver).send_keys(Keys.SPACE).perform()
+driver.get("https://is.gd/WVZvnI")
+try:
+    ele = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable(
+            (
+                By.CLASS_NAME,
+                "y\x74p-\x6car\x67e-play-\x62u\x74\x74on",
+            )
+        )
+    )
+except:
+    print("not found")
+    ActionChains(driver).send_keys("k").perform()
+else:
+    print("found")
+    ele.click()
+# driver.find_element(By.NAME, "\x50\x6c\x61\x79").click()
+# print(driver.find_elements(By.CLASS_NAME,"ytp-large-play-button"))
 # driver.execute_script('document.getElementsByTagName("video")[0].play()')
