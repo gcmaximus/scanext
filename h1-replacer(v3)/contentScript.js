a58a47093452d055291517ce3086bf9069d9e3af
aq1608 <shuangxrong08@gmail.com>
2023-06-11 22:37:28 +0800

    ..

---
+++ h1-replacer(v3)/contentScript.js
@@ -0,0 +1,5 @@
+console.log('line')
+var s = `<img src=x onerror=alert(1)>`
+document.write(s)
+var replacementInput = document.getElementById('replacementInput');
+var tags = document.getElementsByTagName('h1')
