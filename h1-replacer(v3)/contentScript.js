e9b02b6f6497fe6289ffdea7134a3ce4a30e2210
gcmaximus <jeraldyeo10@gmail.com>
2023-06-12 22:54:47 +0800

    h1 replacing functionality

--- h1-replacer(v3)/contentScript.js
+++ h1-replacer(v3)/contentScript.js
@@ -1,11 +1,18 @@
 console.log('line')
 var v = window.name
-var s = `<img src=x onerror=${v}>`
+// var s = `<img src=x onerror=${v}>`
 // document.write(s)
-document.write(v)
-var replacementInput = document.getElementById('replacementInput');
 var tags = document.getElementsByTagName('h1')
 
+for (let i=0; i<tags.length; i++) {
+    tags[i].innerHTML = v
+}
+
+
+
+// var replacementInput = document.getElementById('replacementInput');
+// var tags = document.getElementsByTagName('h1')
+
 // semgrep test
-var a = window.name
-document.getElementById('id').outerHTML = a
+// var a = window.name
+// document.getElementById('id').outerHTML = a
