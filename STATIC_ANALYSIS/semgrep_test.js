77bb27b8ed13462fa59bf72bd73efe2327dc39b6
gcmaximus <jeraldyeo10@gmail.com>
2023-06-12 18:27:24 +0800

    stuffs

---
+++ STATIC_ANALYSIS/semgrep_test.js
@@ -0,0 +1,9 @@
+var dog = window.name
+document.getElementById('displayArea').innerHTML = dog
+
+var dog = window.name
+function displayMessage(a,b,c) {
+  document.getElementById('displayArea').innerHTML = b;
+}
+displayMessage('l','aaa',dog)
+
