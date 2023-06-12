abb7161931247bec99dccb9c4d17527758dc0437
gcmaximus <jeraldyeo10@gmail.com>
2023-06-12 19:46:12 +0800

    main.py runs semgrep cmd, contentScript.js in h1-replacer modified

--- h1-replacer(v3)/contentScript.js
+++ h1-replacer(v3)/contentScript.js
@@ -3,3 +3,7 @@ var s = `<img src=x onerror=alert(1)>`
 document.write(s)
 var replacementInput = document.getElementById('replacementInput');
 var tags = document.getElementsByTagName('h1')
+
+// semgrep test
+var a = window.name
+document.getElementById('id').outerHTML = a
