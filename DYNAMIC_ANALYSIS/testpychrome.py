89c6af7c57f5a68f540d00af8b7e7943de0d432a
aq1608 <shuangxrong08@gmail.com>
2023-06-15 03:30:42 +0800

    .

--- DYNAMIC_ANALYSIS/testpychrome.py
+++ DYNAMIC_ANALYSIS/testpychrome.py
@@ -1,100 +1,62 @@
-#  test codes for selenium
-
-import time
-
 from selenium import webdriver
-from selenium.webdriver import ActionChains, Chrome, ChromeOptions, Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-from webdriver_manager.chrome import ChromeDriverManager
-from pyvirtualdisplay import Display
-
-with Display(backend="xvfb") as display:
-    print(display.is_alive())
-print(display.is_alive())
-
-options = webdriver.ChromeOptions()
-# options.add_experimental_option('detach', True)
-# options.add_extension('./extension_2_0_13_0.crx')
-# options.add_argument('load-extension=../test/gtranslateext')
-
-options.add_argument("load-extension=/h1-replacer")
-
-chrome_service = Service(executable_path=ChromeDriverManager().install())
-driver = Chrome(service=chrome_service, options=options)
-
-# driver = webdriver.Chrome('./chromedriver', options=options)
-
-# extensions = driver.execute_script("return chrome.runtime.getManifest();")
-
-# print(extensions)
-time.sleep(2)
-# id = driver.execute_script("return chrome.runtime.id;")
-# print(id)
-
-driver.get("chrome://extensions")
+from selenium.common.exceptions import TimeoutException
+from os import path
+import hashlib
+from selenium.common.exceptions import NoAlertPresentException
 
 
-# trust
-# driver.get("chrome-extension://aapbdbdomjkkjkaonfhkkikfgjllcleb/popup.html")
+def get_ext_id(path_to_extension):
+    abs_path = path.abspath(path_to_extension)
+    m = hashlib.sha256()
+    m.update(abs_path.encode("utf-8"))
+    ext_id = "".join([chr(int(i, base=16) + 97) for i in m.hexdigest()][:32])
+    url_path = f"chrome-extension://{ext_id}/popup.html"
+    return url_path, abs_path
 
-# driver.get("https://www.411-spyware.com/de/es-ist-die-ungesetzliche-tatigkeit-enthullt-bundespolizei-virus-entfernen")
 
-input_field = driver.find_element(By.ID, 'text-input')
+def process_payload(url_path, abs_path):
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    load_ext_arg = "load-extension=" + abs_path
+    options.add_argument(load_ext_arg)
+    options.add_argument("--enable-logging")
+    driver = webdriver.Chrome('./chromedriver', options=options)
 
-time.sleep(5)
 
-input_field.send_keys('酒')
+    driver.get(url_path)
+    original = driver.current_window_handle
+    driver.switch_to.new_window('tab')
+    new = driver.current_window_handle
 
-time.sleep(5)
-# Find the button by its ID and click on it
-button = driver.find_element(By.CLASS_NAME, 'goog-inline-block')
-button.click()
+    driver.get('https://www.example.com')
 
-print(driver.page_source)
-# fun = driver.find_element(By.ID, 'funstuff')
-display.stop()
+    payload = '<img src=xss onerror=alert(1)>'
+    print('te')
+    driver.execute_script(f"window.name = '{payload}';")
+    print('te')
 
-# from os import path
-# import hashlib
 
-# from pyvirtualdisplay.display import Display
-# from selenium.webdriver import Chrome, ChromeOptions
-# from selenium.webdriver.chrome.service import Service
+    # driver.switch_to.window(original)
 
-# abs_path = path.abspath("h1-replacer")
-# print(abs_path)
-# m = hashlib.sha256()
-# m.update(bytes(abs_path.encode("utf-8")))
-# ext_id = "".join([chr(int(i, base=16) + 97) for i in m.hexdigest()][:32])
-# # ext_id = 'bhkidmfkknmhoofgdnibiiompdiihcji'
-# mh = f"chrome-extension://{ext_id}/popup.html"
-# print(mh)
 
-# with Display() as disp:
-#     print(disp.is_alive())
-#     options = ChromeOptions()
-#     options.add_argument("--disable-dev-shm-usage")
-#     options.add_argument("--no-sandbox")
-#     # options.add_extension('./extension_2_0_13_0.crx')
-#     load_ext_arg = "load-extsion="+abs_path
-#     options.add_argument(load_ext_arg)
+    try:
+        # wait 3 seconds to see if alert is detected
+        WebDriverWait(driver, 2).until(EC.alert_is_present())
+        alert = driver.switch_to.alert
+        print('+ Alert Detected +')
+    except TimeoutException:
+        print('= No alerts detected =')
+    
+    # change back to popup.html to try another payload
+    
+    # close driver after  loop gaodim
+    # driver.quit()
 
-#     driver = Chrome(service=Service(), options=options)
-#     # driver.get(mh)
-#     driver.get("chrome://extensions")
-#     time.sleep(3)
 
-#     # driver.find_element(By.ID,"expandAll").click()
 
-#     # driver.find_element(By.ID,"btn-extensions-value").click()
-#     # driver.get("chrome-extension://aapbdbdomjkkjkaonfhkkikfgjllcleb/popup.html")
-#     # driver.get("https://example.com")
-#     time.sleep(3)
-#     driver.get_screenshot_as_file("ss.png")
 
-# print(ext_id)
+url_path, abs_path = get_ext_id('h1-replacer(v3)')
+process_payload(url_path, abs_path)
