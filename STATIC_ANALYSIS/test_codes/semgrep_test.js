5326b092404a11ad0fce6015c0f4f88a7922d7ed
gcmaximus <jeraldyeo10@gmail.com>
2023-06-15 13:37:32 +0800

    new changes

--- STATIC_ANALYSIS/test_codes/semgrep_test.js
+++ STATIC_ANALYSIS/test_codes/semgrep_test.js
@@ -2,41 +2,42 @@
   1. window.name  
 *******************/ 
 
+// case 1
+var dog = window.name
+document.getElementById('displayArea').innerHTML = dog
 
-// var dog = window.name
-// document.getElementById('displayArea').innerHTML = dog
+// case 2
+var dog = window.name
+function displayMessage(a,b,c) {
+  document.getElementById('displayArea').innerHTML = b;
+}
+displayMessage('l',dog,'k')
 
-// var dog = window.name
-// function displayMessage(a,b,c) {
-//   document.getElementById('displayArea').innerHTML = b;
-// }
-// displayMessage('l',dog,'k')
+// case 3
+var button = document.getElementById('buttonId')
+button.addEventListener('click', function() {
+  var dog = window.name
+  document.getElementById('displayArea').innerHTML = dog
+})
+
+// case 4
+document.getElementById('buttonId').addEventListener('click', function() {
+  var dog = window.name
+  document.getElementById('displayArea').innerHTML = dog
+})
+
+// case 5
+function displayMessage(a,b,c) {
+  document.getElementById('displayArea').innerHTML = b;
+}
+var dog = window.name
+displayMessage('l',dog,'k')
 
 
-// var button = document.getElementById('buttonId')
-// button.addEventListener('click', function() {
-//   var dog = window.name
-//   document.getElementById('displayArea').innerHTML = dog
-// })
-
-
-// document.getElementById('buttonId').addEventListener('click', function() {
-//   var dog = window.name
-//   document.getElementById('displayArea').innerHTML = dog
-// })
-
-
-// var dog = window.name
-// document.getElementById('displayArea').outerHTML = dog
-
-// var dog = window.name
-// function displayMessage(a,b,c) {
-//   document.getElementById('displayArea').outerHTML = b;
-// }
-// displayMessage('l',dog,'k')
-
+// case 7
+var dog = window.name
 $("button").click(function(){
-  $("p").html("Hello <b>world</b>!");
+  $("p").html(dog);
 });
 
 
