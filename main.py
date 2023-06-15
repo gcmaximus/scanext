0a1528497d08eb5086b8dc73cb0ae0efd4536509
gcmaximus <jeraldyeo10@gmail.com>
2023-06-15 15:56:28 +0800

    main modified

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ import subprocess
 
 print("start of program")
 
+# Config rules
+rules = "STATIC_ANALYSIS/semgrep_rules"
+
+# Codes to be scanned
+filename = "STATIC_ANALYSIS/test_codes"
+# filename = "h1-replacer(v3)"
+# filename = "emailextractor"
+
+# Output file
+output_file = "STATIC_ANALYSIS/semgrep_results.json"
+
 
 # auto semgrep scan
 def run_semgrep():
     command = [
         "semgrep",
         "scan",
-        "--config=STATIC_ANALYSIS/semgrep_rules",
-        # "STATIC_ANALYSIS/test_codes",  # replace 
-        # "h1-replacer(v3)",
-        "emailextractor",
+        f"--config={rules}",
+        filename,
         "--output",
-        "STATIC_ANALYSIS/semgrep_results.json",
+        output_file,
         "--json",
         "--quiet",  # turn off verbose
     ]
     try:
         subprocess.run(command, check=True)
         print("Semgrep scan successful.")
-        print("Scan results JSON found in STATIC_ANALYSIS/semgrep_results.json")
+        print(f"JSON scan results of `{filename}` found in `{output_file}`")
     except subprocess.CalledProcessError as e:
         print(f"Error running semgrep command: {e}")
 
