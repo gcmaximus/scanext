8eee1b482271fc983a656c632101f6c23f056ad6
gcmaximus <jeraldyeo10@gmail.com>
2023-06-15 11:45:00 +0800

    semgrep stuff

--- STATIC_ANALYSIS/semgrep_test.js
+++ STATIC_ANALYSIS/semgrep_test.js
@@ -2,6 +2,7 @@
   1. window.name  
 *******************/ 
 
+
 var dog = window.name
 document.getElementById('displayArea').innerHTML = dog
 
@@ -11,6 +12,20 @@ function displayMessage(a,b,c) {
 }
 displayMessage('l',dog,'k')
 
+
+var button = document.getElementById('buttonId')
+button.addEventListener('click', function() {
+  var dog = window.name
+  document.getElementById('displayArea').innerHTML = dog
+})
+
+
+document.getElementById('buttonId').addEventListener('click', function() {
+  var dog = window.name
+  document.getElementById('displayArea').innerHTML = dog
+})
+
+
 var dog = window.name
 document.getElementById('displayArea').outerHTML = dog
 
@@ -19,6 +34,7 @@ function displayMessage(a,b,c) {
   document.getElementById('displayArea').outerHTML = b;
 }
 displayMessage('l',dog,'k')
+
 
 /*******************
   2. location.hash  
