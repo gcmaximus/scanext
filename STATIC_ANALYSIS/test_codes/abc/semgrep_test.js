273b58ae37e674bc353e2ac985a38617c1f7b115
gcmaximus <jeraldyeo10@gmail.com>
2023-06-19 14:43:37 +0800

    input and button separated

--- STATIC_ANALYSIS/test_codes/abc/semgrep_test.js
+++ STATIC_ANALYSIS/test_codes/abc/semgrep_test.js
@@ -3,35 +3,35 @@
 *******************/ 
 
 // case 1
-// var dog = window.name
-// document.getElementById('displayArea').innerHTML = dog
+var dog = window.name
+document.getElementById('displayArea').innerHTML = dog
 
 // case 2
-// var dog = window.name
-// function displayMessage(a,b,c) {
-//   document.getElementById('displayArea').innerHTML = b;
-// }
-// displayMessage('l',dog,'k')
+var dog = window.name
+function displayMessage(a,b,c) {
+  document.getElementById('displayArea').innerHTML = b;
+}
+displayMessage('l',dog,'k')
 
 // case 3
-// var button = document.getElementById('buttonId')
-// button.addEventListener('click', function() {
-//   var dog = window.name
-//   document.getElementById('displayArea').innerHTML = dog
-// })
+var button = document.getElementById('buttonId')
+button.addEventListener('click', function() {
+  var dog = window.name
+  document.getElementById('displayArea').innerHTML = dog
+})
 
 // case 4
-// document.getElementById('buttonId').addEventListener('click', function() {
-//   var dog = window.name
-//   document.getElementById('displayArea').innerHTML = dog
-// })
+document.getElementById('buttonId').addEventListener('click', function() {
+  var dog = window.name
+  document.getElementById('displayArea').innerHTML = dog
+})
 
 // case 5
-// function displayMessage(a,b,c) {
-//   document.getElementById('displayArea').innerHTML = b;
-// }
-// var dog = window.name
-// displayMessage('l',dog,'k')
+function displayMessage(a,b,c) {
+  document.getElementById('displayArea').innerHTML = b;
+}
+var dog = window.name
+displayMessage('l',dog,'k')
 
 // case 6
 document.getElementById('buttonId').addEventListener('click', function() {
@@ -49,3 +49,4 @@ document.getElementById('buttonId').addEventListener('click', function() {
 /*******************
   2. location.hash  
 *******************/ 
+
