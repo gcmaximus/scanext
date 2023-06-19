946f205c231cdfd4f66438127797f04fa519f4c2
gcmaximus <jeraldyeo10@gmail.com>
2023-06-19 20:44:57 +0800

    more rules

--- STATIC_ANALYSIS/test_codes/abc/semgrep_test.js
+++ STATIC_ANALYSIS/test_codes/abc/semgrep_test.js
@@ -1,33 +1,345 @@
 /*******************
   1. window.name  
 *******************/ 
-// innerHTML
-// case 1
-var case1 = window.name
-document.getElementById('case1').innerHTML = case1
+// // innerHTML
+// // case 1
+// var case1 = window.name
+// document.getElementById('case1').innerHTML = case1
 
-// case 2
-document.getElementById('case2').innerHTML = window.name
+// // case 2
+// var case2 = window.name
+// newcase2= case2 + " "
+// document.getElementById('case2').innerHTML = newcase2
 
-// case 3
-var case3 = window.name
-newcase3 = case3 + " "
-document.getElementById('case3').innerHTML = newcase3
+// // case 3
+// document.getElementById('case3').innerHTML = window.name
 
-//outerHTML
-// case 4
-var case4 = window.name
-document.getElementById('case4').innerHTML = case4
+//**************************************************************/
 
-// case 5
-document.getElementById('case5').innerHTML = window.name
+// //outerHTML
+// // case 4
+// var case4 = window.name
+// document.getElementById('case4').innerHTML = case4
 
-// case 6
-var case6 = window.name
-newcase6 = case6 + " "
-document.getElementById('case6').innerHTML = newcase6
+// // case 5
+// var case5 = window.name
+// newcase5 = case5 + " "
+// document.getElementById('case5').innerHTML = newcase5
 
-/*******************
-  2. location.hash  
-*******************/ 
+// // case 6
+// document.getElementById('case6').innerHTML = window.name
 
+//**************************************************************/
+
+// // document.write
+// // case 7
+// var case7 = window.name
+// document.write(case7)
+
+// // case 8 
+// var case8 = window.name
+// newcase8 = " " + case8
+// document.write(newcase8)
+
+// // Case 9
+// document.write(window.name)
+
+//**************************************************************/
+
+// // document.writeln
+// // Case 10
+// var case10 = window.name
+// document.writeln(case10)
+
+// // Case 11
+// var case11 = window.name
+// newcase11 = " " + case11
+// document.writeln(newcase11)
+
+// // Case 12
+// document.writeln(window.name)
+
+//**************************************************************/
+
+// // jQuery.html()
+// // Case 13
+// var case13 = window.name;
+// $("#case13").html(case13);
+
+// // Case 14
+// var case14 = window.name;
+// newcase14 = " " + case14;
+// $("#case14").html(newcase14);
+
+// // Case 15
+// $("#case15").html(window.name);
+
+//**************************************************************/
+
+// // jQuery.before()
+// // Case 16
+// var case16 = window.name;
+// $("#case16").before(case16);
+
+// // Case 17
+// var case17 = window.name;
+// newcase17 = " " + case17;
+// $("#case17").before(newcase17);
+
+// // Case 18
+// $("#case18").before(window.name);
+
+//**************************************************************/
+
+// // jQuery .insertBefore()
+// // Case 19
+// var case19 = window.name;
+// $("<div>" + case19 + "</div>").insertBefore("#element");
+
+// // Case 20
+// var case20 = window.name;
+// newcase20 = " " + case20;
+// $("<div>" + case20 + "</div>").insertBefore("#element");
+
+// // Case 21
+// $("<div>" + window.name + "</div>").insertBefore("#element");
+
+//**************************************************************/
+
+// // jQuery .insertAfter()
+// // Case 22
+// var case22 = window.name;
+// $("<div>" + case22 + "</div>").insertAfter("#element");
+
+// // Case 23
+// var case23 = window.name;
+// newcase23 = " " + case23;
+// $("<div>" + case23 + "</div>").insertAfter("#element");
+
+// // Case 24
+// $("<div>" + window.name + "</div>").insertAfter("#element");
+
+//**************************************************************/
+
+// // jQuery .after()
+// // Case 25
+// var case25 = window.name;
+// $("#element").after("<div>" + case25 + "</div>");
+
+// // Case 26
+// var case26 = window.name;
+// newcase26 = " " + case26;
+// $("#element").after("<span>" + newcase26 + "</span>");
+
+// // Case 27
+// $("#element").after("<p>" + window.name + "</p>");
+
+//**************************************************************/
+
+// // jQuery .append()
+// // Case 28
+// var case28 = window.name;
+// $("#element").append("<div>" + case28 + "</div>");
+
+// // Case 29
+// var case29 = window.name;
+// newcase29 = " " + case29;
+// $("#element").append("<span>" + newcase29 + "</span>");
+
+// // Case 30
+// $("#element").append("<p>" + window.name + "</p>");
+
+//**************************************************************/
+
+// // jQuery .prepend()
+// // Case 31
+// var case31 = window.name;
+// $("#element").prepend("<div>" + case31 + "</div>");
+
+// // Case 32
+// var case32 = window.name;
+// newcase32 = " " + case32;
+// $("#element").prepend("<span>" + newcase32 + "</span>");
+
+// // Case 33
+// $("#element").prepend("<p>" + window.name + "</p>");
+
+//**************************************************************/
+
+// // jQuery .wrap()
+// // Case 34
+// var case34 = window.name;
+// $("#element").wrap("<div class='wrapper'>" + case34 + "</div>");
+
+// // Case 35
+// var case35 = window.name;
+// newcase35 = " " + case35;
+// $("#element").wrap("<span class='wrapper'>" + newcase35 + "</span>");
+
+// // Case 36
+// $("#element").wrap("<p class='wrapper'>" + window.name + "</p>");
+
+//**************************************************************/
+
+// // jQuery .wrapAll()
+// // Case 37
+// var case37 = window.name;
+// $(".elements").wrapAll("<div class='wrapper'>" + case37 + "</div>");
+
+// // Case 38
+// var case38 = window.name;
+// newcase38 = " " + case38;
+// $(".elements").wrapAll("<span class='wrapper'>" + newcase38 + "</span>");
+
+// // Case 39
+// $(".elements").wrapAll("<p class='wrapper'>" + window.name + "</p>");
+
+//**************************************************************/
+
+// // jQuery .wrapInner()
+// // Case 40
+// var case40 = window.name;
+// $("#element").wrapInner("<div class='wrapper'>" + case40 + "</div>");
+
+// // Case 41
+// var case41 = window.name;
+// newcase41 = " " + case41;
+// $("#element").wrapInner("<span class='wrapper'>" + newcase41 + "</span>");
+
+// // Case 42
+// $("#element").wrapInner("<p class='wrapper'>" + window.name + "</p>");
+
+//**************************************************************/
+
+// // jQuery .appendTo()
+// // Case 43
+// var case43 = window.name;
+// $("<div>" + case43 + "</div>").appendTo("#target");
+
+// // Case 44
+// var case44 = window.name;
+// newcase44 = " " + case44;
+// $("<span>" + newcase44 + "</span>").appendTo("#target");
+
+// // Case 45
+// $("<p>" + window.name + "</p>").appendTo("#target");
+
+//**************************************************************/
+
+// // jQuery .prependTo()
+// // Case 46
+// var case46 = window.name;
+// $("<div>" + case46 + "</div>").prependTo("#target");
+
+// // Case 47
+// var case47 = window.name;
+// newcase47 = " " + case47;
+// $("<span>" + newcase47 + "</span>").prependTo("#target");
+
+// // Case 48
+// $("<p>" + window.name + "</p>").prependTo("#target");
+
+//**************************************************************/
+
+// // jQuery .attr("href")
+// // Case 49
+// var case49 = window.name;
+// $("a").attr("href", case49);
+
+// // Case 50
+// var case50 = window.name;
+// newcase50 = "javascript:" + case50;
+// $("a").attr("href", newcase50);
+
+// // Case 51
+// $("a").attr("href", "javascript:" + window.name);
+
+//**************************************************************/
+
+// setAttribute("href")
+// Case 52
+var case52 = window.name;
+document.getElementById("link").setAttribute("href", case52);
+
+// Case 53
+var case53 = window.name;
+newcase53 = "javascript:" + case53;
+document.getElementById("link").setAttribute("href", newcase53);
+
+// Case 54
+document.getElementById("link").setAttribute("href", "javascript:" + window.name);
+
+//**************************************************************/
+
+// setAttribute("src")
+// Case 55
+var case55 = window.name;
+document.getElementById("iframe").setAttribute("src", case55);
+
+// Case 56
+var case56 = window.name;
+newcase56 = "javascript:" + case56;
+document.getElementById("iframe").setAttribute("src", newcase56);
+
+// Case 57
+document.getElementById("iframe").setAttribute("src", "javascript:" + window.name);
+
+//**************************************************************/
+
+// chrome.tabs.update
+// Case 58
+var case58 = window.name;
+chrome.tabs.update({ url: case58 });
+
+// Case 59
+var case59 = window.name;
+newcase59 = "javascript:" + case59;
+chrome.tabs.update({ url: newcase59 });
+
+// Case 60
+chrome.tabs.update({ url: "javascript:" + window.name });
+
+//**************************************************************/
+
+// // chrome.tabs.executeScript
+// // Case 61
+// var case61 = window.name;
+// chrome.tabs.executeScript(null, { code: case61 });
+
+// // Case 62
+// var case62 = window.name;
+// newcase62 = "javascript:" + case62;
+// chrome.tabs.executeScript(null, { code: newcase62 });
+
+// // Case 63
+// chrome.tabs.executeScript(null, { code: "javascript:" + window.name });
+
+//**************************************************************/
+
+// // eval()
+// // Case 64
+// var case64 = window.name;
+// eval(case64);
+
+// // Case 65
+// var case65 = window.name;
+// newcase65 = "javascript:" + case65;
+// eval(newcase65);
+
+// // Case 66
+// eval("javascript:" + window.name);
+
+//**************************************************************/
+
+// // jQuery globalEval()
+// // Case 67
+// var case67 = window.name;
+// jQuery.globalEval(case67);
+
+// // Case 68
+// var case68 = window.name;
+// newcase68 = "javascript:" + case68;
+// jQuery.globalEval(newcase68);
+
+// // Case 69
+// jQuery.globalEval("javascript:" + window.name);
