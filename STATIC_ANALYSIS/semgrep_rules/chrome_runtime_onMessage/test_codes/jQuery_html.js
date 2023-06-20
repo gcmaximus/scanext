00f113f0c3b2bb112960dd856af40a3370af8eab
Your Name <rifqikhuzairi@gmail.com>
2023-06-21 02:22:35 +0800

    modify the test codes file names and added jquery yaml for chromeruntime

---
+++ STATIC_ANALYSIS/semgrep_rules/chrome_runtime_onMessage/test_codes/jQuery_html.js
@@ -0,0 +1,31 @@
+//LINES 4-9 is an example of injecting HTML from the content script to the current page
+// Content Script
+// Listen for messages from the background script or popup
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'injectHTML') {
+      var htmlContent = message.html;
+      $('body').html(htmlContent);
+    }
+  });
+  //LINES 13-19 is an example of receiving HTML from the background script and updating a specific element
+  // Content Script
+  // Listen for messages from the background script or popup
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'updateElement') {
+      var elementSelector = message.selector;
+      var htmlContent = message.html;
+      $(elementSelector).html(htmlContent);
+    }
+  });
+  //LINES 23-31 is an example of updating multiple elements with HTML content
+  // Content Script
+  // Listen for messages from the background script or popup
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'updateElements') {
+      var elementSelector = message.selector;
+      var htmlContent = message.html;
+      $(elementSelector).each(function() {
+        $(this).html(htmlContent);
+      });
+    }
+  });
