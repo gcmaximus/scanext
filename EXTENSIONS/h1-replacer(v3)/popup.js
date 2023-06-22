341762b884e789256e96f1d7c463d0ca98e55f53
gcmaximus <jeraldyeo10@gmail.com>
2023-06-22 23:45:59 +0800

    ext

--- EXTENSIONS/h1-replacer(v3)/popup.js
+++ EXTENSIONS/h1-replacer(v3)/popup.js
@@ -1,5 +1,7 @@
 async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
+  let queryOptions = { active: false, lastFocusedWindow: true };
+  // let queryOptions = { active: true, lastFocusedWindow: true };
+
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
@@ -7,7 +9,7 @@ async function getCurrentTab() {
 
 getCurrentTab().then((tab) => {
 
-  document.getElementsByTagName("h1")[0].innerHTML = tab.title
+  // document.getElementsByTagName("h1")[0].innerHTML = tab.title
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ["contentScript.js"]
