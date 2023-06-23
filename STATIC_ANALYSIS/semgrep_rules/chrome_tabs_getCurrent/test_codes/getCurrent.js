7f292309ac6ba8f239710d3b2720dea8846c950b
gcmaximus <jeraldyeo10@gmail.com>
2023-06-23 17:16:00 +0800

    chrome_tabs_getCurrent

---
+++ STATIC_ANALYSIS/semgrep_rules/chrome_tabs_getCurrent/test_codes/getCurrent.js
@@ -0,0 +1,18 @@
+async function updateTabUrl(tabId, newUrl) {
+  try {
+    const tab = await chrome.tabs.getCurrent()
+    if (tab) {
+      // Update the URL property of the retrieved tab using chrome.tabs.update
+      const current = await chrome.tabs.update(tab.id, { url: tab.url });
+      console.log("Tab URL updated:", updatedTab);
+    } else {
+      console.log("Tab not found");
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
+
+// Usage: Call the function and pass the tab ID and new URL as arguments
+const tabId = 123; // Replace with the actual tab ID
+updateTabUrl(tabId, newUrl);
