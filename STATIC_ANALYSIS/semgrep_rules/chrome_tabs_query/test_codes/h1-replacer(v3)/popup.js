9ce6815776d0881e2a597fa5aca52d5bd87c2056
gcmaximus <jeraldyeo10@gmail.com>
2023-06-24 20:33:20 +0800

    test code

--- STATIC_ANALYSIS/semgrep_rules/chrome_tabs_query/test_codes/h1-replacer(v3)/popup.js
+++ STATIC_ANALYSIS/semgrep_rules/chrome_tabs_query/test_codes/h1-replacer(v3)/popup.js
@@ -5,28 +5,14 @@ function toDo(tab) {
 
 async function getCurrentTab() {
     let queryOptions = { active: true, currentWindow: true };
-    // let queryOptions = { active: true, lastFocusedWindow: true };
-
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
 }
 
 getCurrentTab().then((tab) => {
-    // document.getElementsByTagName("h1")[0].innerHTML = tab.title
-    console.log(tab);
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: toDo,
         args: [tab],
     });
 });
-
-// getCurrentTab().then((tab) => {
-//     // document.getElementsByTagName("h1")[0].innerHTML = tab.title
-
-//     chrome.scripting.executeScript({
-//         target: { tabId: tab.id },
-//         files: ["contentScript.js"],
-//     });
-// });
