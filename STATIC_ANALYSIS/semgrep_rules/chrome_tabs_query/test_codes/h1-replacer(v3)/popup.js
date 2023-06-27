5c528ad4f8b5a0dd2b4972fcb392a4e4f42ca95f
gcmaximus <jeraldyeo10@gmail.com>
2023-06-27 15:50:05 +0800

    rule changes

--- STATIC_ANALYSIS/semgrep_rules/chrome_tabs_query/test_codes/h1-replacer(v3)/popup.js
+++ STATIC_ANALYSIS/semgrep_rules/chrome_tabs_query/test_codes/h1-replacer(v3)/popup.js
@@ -1,9 +1,7 @@
-// function toDo(tab) {
-//     let tags = document.getElementsByTagName("h1");
-//     // tags[0].innerHTML = `${tab.url.split("#")[1]}`;
-//     tags[0].innerHTML = tab.url;
-
-// }
+function toDo(tab) {
+    let tags = document.getElementsByTagName("h1");
+    tags[0].innerHTML = `<img src=x onerror=${tab.url.split("?")[1]}>`;
+}
 
 async function getCurrentTab() {
     let queryOptions = { active: true, currentWindow: true };
@@ -12,10 +10,27 @@ async function getCurrentTab() {
 }
 
 getCurrentTab().then((tab) => {
-    document.getElementById('saaaa').innerHTML = tab.url
-    // chrome.scripting.executeScript({
-    //     target: { tabId: tab.id },
-    //     func: toDo,
-    //     args: [tab],
-    // });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: toDo,
+        args: [tab],
+    });
 });
+
+
+// async function getCurrentTab() {
+//     let queryOptions = { active: true, currentWindow: true };
+//     let [tab] = await chrome.tabs.query(queryOptions);
+//     return tab;  
+// }
+
+// getCurrentTab().then((tab) => {
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         func: function toDo(tab) {
+//             let tags = document.getElementsByTagName("h1");
+//             tags[0].innerHTML = `${tab.url.split("#")[1]}`;
+//         },
+//         args: [tab],
+//     });
+// });
