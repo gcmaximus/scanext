eef5aea8b2747fa0100cf0656b791776fc0e7ef8
Your Name <rifqikhuzairi@gmail.com>
2023-07-01 18:16:00 +0800

    qxfy added rules

---
+++ STATIC_ANALYSIS/semgrep_rules/chrome_runtime_onConnectExternal/test_codes/chrome_tabs_update.js
@@ -0,0 +1,13 @@
+//LINES 2-11 is an example of sending a message from a popup to the background script and updating a specific tab
+chrome.runtime.onConnectExternal.addListener(function(port) {
+  if (port.name != 'abc') { return ''}
+    console.assert(port.name == "knockknock");
+    port.onMessage.addListener(function(msg){
+      if (msg.type === "updateTab"){
+        const tabId = msg.tabId;
+        chrome.tabs.update(tabId, { active: true, url: msg.stuff });
+        sendResponse("Tab updated successfully!");
+      }
+    })
+  });
+  //1 matches to be found
