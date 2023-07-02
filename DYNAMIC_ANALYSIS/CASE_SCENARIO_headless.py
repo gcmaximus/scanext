6078162fb6e3a2b51cf7e8bbff17ca1c3dd1d804
aq1608 <shuangxrong08@gmail.com>
2023-07-02 15:28:58 +0800

    edited headless case scenarios

--- DYNAMIC_ANALYSIS/CASE_SCENARIO_headless.py
+++ DYNAMIC_ANALYSIS/CASE_SCENARIO_headless.py
@@ -1,6 +1,6 @@
 import json
 
-f = open('s.json')
+f = open('j.json')
 
 results = json.load(f)
 
@@ -75,55 +75,59 @@ def runtime_onC(extid, payload, ssm, msgvar):
         scripts.append(script)
 
 def runtime_onM(extid, payload, ssm, msgvar):
-
-    html = 'rHTML'
-    dots = '.'
-    underscore = '_'
-    message = ssm[0]["message"]
-    if html in message:
-        sink_split = message.split('Sink:')
-        sink = sink_split[-1]
-    elif dots in message:
-        sink_split = message.split(dots)
-        sink = sink_split[-1]
-    elif underscore in message:
-        sink_split = message.split(underscore)
-        sink = sink_split[-1]
-    
-    taintsink = ssm[0]["sink"]
-    
-    varindex = taintsink.find(sink+"(") + sink.__len__() + 1
-    for i in msgvar:
-        msgindex = taintsink.find(i)
-        if msgindex == -1:
-            continue
-        elif varindex == msgindex:
-            #source is here
-            endvarindex = taintsink.find(")",varindex)
-            if endvarindex == -1:
-                endvarindex = 0
-            source = taintsink[varindex:endvarindex]
-            if dots in source:
-                sourcel = source.split(dots)
-                obj = {sourcel[1]:payload}
+    for k in ssm:
+        html = 'rHTML'
+        dots = '.'
+        underscore = '_'
+        message = k["message"]
+        if html in message:
+            sink_split = message.split('Sink:')
+            sink = sink_split[-1]
+        elif dots in message:
+            sink_split = message.split(dots)
+            sink = sink_split[-1]
+        elif underscore in message:
+            sink_split = message.split(underscore)
+            sink = sink_split[-1]
+        
+        taintsink = k["sink"]
+        
+        varindex = taintsink.find(sink+"(") + sink.__len__() + 1
+        for i in msgvar[ssm.index(k)]["content"]:
+            msgindex = taintsink.find(i)
+            if msgindex == -1:
+                continue
+            elif varindex == msgindex:
+                #source is here
+                endvarindex = taintsink.find(")",varindex)
+                if endvarindex == -1:
+                    endvarindex = 0
+                source = taintsink[varindex:endvarindex]
+                if dots in source:
+                    sourcel = source.split(dots)
+                    obj = {sourcel[1]:payload}
+                    obj = json.dumps(obj)
+                    var = f"obj = JSON.parse('{obj}');"
+                else:
+                    var = f"obj = '{payload}';"
             else:
-                obj = {payload}
-        else:
-            endvarindex = taintsink.find(")",msgindex)
-            if endvarindex == -1:
-                endvarindex = 0
-            source = taintsink[msgindex:endvarindex]
-            if dots in source:
-                sourcel = source.split(dots)
-                obj = {sourcel[1]:payload}
-            else:
-                obj = {payload}
-    
-    script = f'obj = JSON.parse("{obj}");chrome.runtime.sendMessage({extid},obj)'
-    return script
+                endvarindex = taintsink.find(")",msgindex)
+                if endvarindex == -1:
+                    endvarindex = 0
+                source = taintsink[msgindex:endvarindex]
+                if dots in source:
+                    sourcel = source.split(dots)
+                    obj = {sourcel[1]:payload}
+                    obj = json.dumps(obj)
+                    var = f"obj = JSON.parse('{obj}');"
+                else:
+                    var = f"obj = '{payload}';"
+        
+        script = f"{var}chrome.runtime.sendMessage({extid},obj)"
+        scripts.append(script)
 
 for i in data:
-    if "chrome_runtime_onMessage" in i["check_id"]:
+    if "chrome_runtime_onMessage." in i["check_id"]:
         taint = {}
         taint_sink = i["extra"]["dataflow_trace"]["taint_sink"][1][1]
         taint_source = i["extra"]["dataflow_trace"]["taint_source"][1][1]
@@ -136,9 +140,8 @@ for i in data:
         taint["source"] = taint_source
         taint["sink"] = taint_sink
         tainted.append(taint)
-        print(runtime_onM("extid","<img src=x onerror=alert(1)>",tainted,metavars))
 
-    if "chrome_runtime_onConnect" in i["check_id"]:
+    if "chrome_runtime_onConnect." in i["check_id"]:
         taint = {}
         taint_sink = i["extra"]["dataflow_trace"]["taint_sink"][1][1]
         taint_source = i["extra"]["dataflow_trace"]["taint_source"][1][1]
@@ -156,8 +159,43 @@ for i in data:
         taint["source"] = taint_source
         taint["sink"] = taint_sink
         tainted.append(taint)
+    
+    if 'chrome_cookies_get.' in i["check_id"]:
+        taint = {}
+        taint_sink = i["extra"]["dataflow_trace"]["taint_sink"][1][1]
+        taint_source = i["extra"]["dataflow_trace"]["taint_source"][1][1]
+        metavars = []
+        try:
+            if i["extra"]["metavars"]["$COOKIE"]:
+                metavars.append(i["extra"]["metavars"]["$COOKIE"]["abstract_content"])
+            if i["extra"]["metavars"]["$DETAILS"]:
+                metavars.append(i["extra"]["metavars"]["$DETAILS"]["abstract_content"])
+            if i["extra"]["metavars"]["$FUNC"]:
+                metavars.append(i["extra"]["metavars"]["$FUNC"]["abstract_content"])
+        except:
+            print("no function")
+        try:
+            if i["extra"]["metavars"]["$X"]:
+                metavars.append(i["extra"]["metavars"]["$X"]["abstract_content"])
+            if i["extra"]["metavars"]["$W"]:
+                metavars.append(i["extra"]["metavars"]["$W"]["abstract_content"])
+        except:
+            print("no w")
+        try:
+            if i["extra"]["metavars"]["$Y"]:
+                metavars.append(i["extra"]["metavars"]["$Y"]["abstract_content"])
+        except:
+            print("no y")
+        
+        other_vars.append({"content":metavars})
+        message = i["extra"]["message"]
+        taint["message"] = message
+        taint["source"] = taint_source
+        taint["sink"] = taint_sink
+        tainted.append(taint)
 
-runtime_onC('extid','<img src=x onerror=alert(1)>',tainted,metavars)
+
+runtime_onM("extid","<img src=x onerror=alert(1)>",tainted,other_vars)
+
 for i in scripts:
     print(i)
-
