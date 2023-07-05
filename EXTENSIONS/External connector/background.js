725f4f007c5c073eb12a706d4934fa37db38a054
qxfy <rifqikhuzairi@gmail.com>
2023-07-05 15:34:11 +0800

    qxfy modify extension and rule

--- EXTENSIONS/External connector/background.js
+++ EXTENSIONS/External connector/background.js
@@ -1,23 +1,23 @@
-chrome.runtime.onConnectExternal.addListener((port) => {
-    if (port.name === 'replaceH1Port') {
-        port.onMessage.addListener((message) => {
-            if (message.action === 'replaceH1') {
-                chrome.scripting.executeScript({
-                    target: { tabId: port.sender.tab.id },
-                    function: replaceH1,
-                    args: [message.text]
-                });
-            }
-        });
-    }
-});
+// chrome.runtime.onConnectExternal.addListener((port) => {
+//     if (port.name === 'replaceH1Port') {
+//         port.onMessage.addListener((message) => {
+//             if (message.action === 'replaceH1') {
+//                 chrome.scripting.executeScript({
+//                     target: { tabId: port.sender.tab.id },
+//                     function: replaceH1,
+//                     args: [message.text]
+//                 });
+//             }
+//         });
+//     }
+// });
 
-function replaceH1(text) {
-    const h1Element = document.querySelector('h1');
-    if (h1Element) {
-        h1Element.innerHTML = text;
-    }
-}
+// function replaceH1(text) {
+//     const h1Element = document.querySelector('h1');
+//     if (h1Element) {
+//         h1Element.innerHTML = text;
+//     }
+// }    RIFQI COMMENTED LINE 1 TO 20
 // send this from a external extension/console
 // const extensionId = 'YOUR_EXTENSION_ID'; // Replace with your extension's ID
 
@@ -27,3 +27,24 @@ function replaceH1(text) {
 //         text: 'Hello from external source!'
 //     });
 // });
+chrome.scripting.executeScript({
+    target: { tabId: port.sender.tab.id },
+    function: replaceH1,
+    args: [message.text]
+});
+
+function replaceH1(text) {
+    chrome.runtime.onConnectExternal.addListener((port) => {
+    if (port.name === 'replaceH1Port') {
+        port.onMessage.addListener((message) => {
+            if (message.action === 'replaceH1') {
+              const h1Element = document.querySelector('h1');
+              if (h1Element) {
+                  h1Element.innerHTML = text;
+              }
+            }
+        });
+    }
+});
+}
+// RIFQI added lines 30- 50
