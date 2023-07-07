ad0e1283fb475b68494de39d6b5ffbae825b006e
Dei-Filius <hwdkong@hotmail.sg>
2023-07-07 16:56:42 +0800

    update main

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import json
 import shutil
 import subprocess
+from email.utils import formatdate
 from os import makedirs
 from pathlib import Path
 from zipfile import ZipFile
-from DYNAMIC_ANALYSIS.headless_cases import main as dynamic
-from email.utils import formatdate
+
 import jsbeautifier
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+
+from DYNAMIC_ANALYSIS.headless_cases import main as dynamic
 
 
 # extract extensions and format the extracted files
@@ -44,20 +46,22 @@ def extraction():
                 pretty = jsbeautifier.beautify_file(file, opts)
                 local_options = jsbeautifier.BeautifierOptions()
                 local_options.keep_quiet = True
-                jsbeautifier.write_beautified_output(
-                    pretty, local_options, str(file)
-                )
-    return Path(extraction_dir).glob('*')
+                jsbeautifier.write_beautified_output(pretty, local_options, str(file))
+    return Path(extraction_dir).glob("*")
 
 
-    
-
-def static_analysis(extension: Path, soup):
+def static_analysis(extension: Path, soup: BeautifulSoup):
     # Config rules
     rules = "STATIC_ANALYSIS/semgrep_rules/"
 
     # Output file
     output_file = "STATIC_ANALYSIS/semgrep_results.json"
+
+    # Name of folder scanned
+    scanned_dir = extension.name
+
+    # descriptions from Tarnish
+    descs = "SHARED/descriptions.json"
 
     # Command to run on CLI
     command = [
@@ -66,71 +70,56 @@ def static_analysis(extension: Path, soup):
         f"--config={rules}",
         str(extension),
         "--quiet",
-        "--json", 
+        "--json",
         "--output",
-        output_file
+        output_file,
     ]
 
     try:
         subprocess.run(command, check=True)
         print("Semgrep scan successful")
-        print(f"JSON scan results of `{extension.name}` found in `{output_file}`")
+        print(f"JSON scan results of `{scanned_dir}` found in `{output_file}`")
     except subprocess.CalledProcessError as e:
         print(f"Error running semgrep command: {e}")
         exit()
 
     # read the static results
     with open(output_file, "r") as static_result_file:
-        results = json.load(static_result_file)["results"]
+        results: list[dict] = json.load(static_result_file)["results"]
         sorted_results = sorted(results, key=lambda e: e["path"])
         sorted_results = sorted(sorted_results, key=lambda e: e["check_id"])
 
-
-
     # Retrieving information from Static Analysis for report
 
-    # Name of folder scanned
-    folder_scanned = extension.name
-
     # Information from manifest.json
-    path_to_manifest = tuple(Path(extension).glob("**/manifest.json"))[0]
+    [manifest_path] = tuple(Path(extension).glob("**/manifest.json"))
 
-    with open(path_to_manifest, 'r') as f:
-        content = json.load(f)
+    with open(manifest_path, "r") as f:
+        manifest = json.load(f)
 
         # Extract information
-        ext_name = str(content["name"])
-        ext_version = str(content["version"])
-        manifest_version = str(content["manifest_version"])
-
+        ext_name = str(manifest["name"])
+        ext_version = str(manifest["version"])
+        manifest_version = str(manifest["manifest_version"])
 
     # No of vulnerabilities found
-    no_of_vulns = str(len(results))
-    
+    vulns_len = len(results)
+
     # No of POCs
-    # no_of_pocs = 
+    # no_of_pocs =
 
+    soup.find(id="scanned-folder").string = scanned_dir
+    soup.find(id="ext-name").string = ext_name
+    soup.find(id="ext-version").string = ext_version
 
-    html_scanned_folder = soup.find(id="scanned-folder")
-    html_scanned_folder.string = folder_scanned
-
-    html_ext_name = soup.find(id="ext-name")
-    html_ext_name.string = ext_name
-
-    html_ext_version = soup.find(id="ext-version")
-    html_ext_version.string = ext_version
-
-    html_manifest_version = soup.find(id="manifest-version")
-    html_manifest_version.string = manifest_version
-
-    html_vulns = soup.find(id="vulns")
-    html_vulns.string = no_of_vulns + ' found'
+    soup.find(id="manifest-version").string = manifest_version
+    soup.find(id="vulns").string = str(vulns_len) + " found"
 
     # html_pocs = soup.find(id="pocs")
-    # html_pocs.string = 
+    # html_pocs.string =
 
     # append semgrep info to report
-    if len(results) == 0:
+    if vulns_len == 0:
         print("sibei secure")
 
     else:
@@ -138,157 +127,134 @@ def static_analysis(extension: Path, soup):
         result_no = 1
         for result in results:
             print()
-            vuln_id = result['check_id'].split('.')[-1]
-            vuln_file = result['path'].split('SHARED/EXTRACTED/')[1]
+            vuln_id = result["check_id"].split(".")[-1]
+            vuln_file = Path(result["path"])
 
-            source, sink = vuln_id.replace('_','.').split('-')
+            source, sink = vuln_id.replace("_", ".").split("-")
 
             # find desc for source & sink
-            with open("SHARED/descriptions.json", "r") as f:
+            with open(descs, "r") as f:
                 content = json.load(f)
                 source_desc = content["sources"][source]
                 sink_desc = content["sinks"][sink]
 
-
             # find line no. of vuln + the line itself
 
             # get SS line numbers
-            source_line_no = result['extra']['dataflow_trace']['taint_source'][1][0]['start']['line']
-
-            sink_line_no = result['extra']['dataflow_trace']['taint_sink'][1][0]['start']['line']
+            dataflow_trace: dict = result["extra"]["dataflow_trace"]
+            source_line_no = dataflow_trace["taint_source"][1][0]["start"]["line"]
+            sink_line_no = dataflow_trace["taint_sink"][1][0]["start"]["line"]
 
             print("Source Line: " + str(source_line_no))
             print("Sink Line: " + str(sink_line_no))
 
-            # get SS lines
-            file = open(f'SHARED/EXTRACTED/{vuln_file}')
-
-            vulnerable_lines = {
-                "source_line": "",
-                "sink_line": ""
-            }
-            for i, line in enumerate(file):
-                if i == source_line_no-1:
-                    vulnerable_lines["source_line"] = line.strip()
-                
-                if i == sink_line_no-1:
-                    vulnerable_lines["sink_line"] = line.strip()
-
-            print(vulnerable_lines)
-
+            # get source and sink lines
+            vulnerable_lines = {}
+            with open(vuln_file, "r") as f:
+                for i, line in enumerate(f):
+                    if i == source_line_no - 1:
+                        setattr(vulnerable_lines, "source_line", line.strip())
+                    if i == sink_line_no - 1:
+                        setattr(vulnerable_lines, "sink_line", line.strip())
+                    if len(vulnerable_lines) == 2:
+                        break
+            source_line = vulnerable_lines.setdefault("source_line", "")
+            sink_line = vulnerable_lines.setdefault("sink_line", "")
             line_diff = abs(source_line_no - sink_line_no)
-
-            print('line diff: ', line_diff)
-            
+            print(vulnerable_lines)
+            print("line diff: ", line_diff)
 
             # check if:
             # 3. line difference < 1?
+            code_segment = ""
             if line_diff < 1:
-                code_segment = f'''
+                code_segment = f"""
 <pre class="code-block">
                     <code class="code-source">
-    {source_line_no}&#9;&#9;<mark id="code-source-{result_no}">{vulnerable_lines['source_line']}</mark>&#9;<span class="code-comment">/* Source + Sink */</span></code>
-                </pre>'''
+    {source_line_no}&#9;&#9;<mark id="code-source-{result_no}">{source_line}</mark>&#9;<span class="code-comment">/* Source + Sink */</span></code>
+                </pre>"""
 
             # 4. line difference == 1?
             elif line_diff == 1:
-                code_segment = f'''
+                code_segment = f"""
 <pre class="code-block">
                     <code class="code-source">
-    {source_line_no}&#9;&#9;<mark id="code-source-{result_no}">{vulnerable_lines['source_line']}</mark>&#9;<span class="code-comment">/* Source */</span></code><code>
-    {sink_line_no}&#9;&#9;<mark id="code-sink-{result_no}">{vulnerable_lines['sink_line']}</mark>&#9;<span class="code-comment">/* Sink */</span></code>
-                </pre>'''
+    {source_line_no}&#9;&#9;<mark id="code-source-{result_no}">{source_line}</mark>&#9;<span class="code-comment">/* Source */</span></code><code>
+    {sink_line_no}&#9;&#9;<mark id="code-sink-{result_no}">{sink_line}</mark>&#9;<span class="code-comment">/* Sink */</span></code>
+                </pre>"""
 
             # 5. line difference > 1?
             elif line_diff > 1:
-                code_segment = f'''
+                code_segment = f"""
 <pre class="code-block">
                     <code class="code-source">
-    {source_line_no}&#9;&#9;<mark id="code-source-{result_no}">{vulnerable_lines['source_line']}</mark>&#9;<span class="code-comment">/* Source */</span></code><code>
+    {source_line_no}&#9;&#9;<mark id="code-source-{result_no}">{source_line}</mark>&#9;<span class="code-comment">/* Source */</span></code><code>
     ...&#9;&#9;...</code><code class="code-sink">
-    {sink_line_no}&#9;&#9;<mark id="code-sink-{result_no}">{vulnerable_lines['sink_line']}</mark>&#9;<span class="code-comment">/* Sink */</span></code>
-                </pre>'''
+    {sink_line_no}&#9;&#9;<mark id="code-sink-{result_no}">{sink_line}</mark>&#9;<span class="code-comment">/* Sink */</span></code>
+                </pre>"""
 
             # 6. intermediate vars > 1? (detailed tainted path)
-            try:
-                vars = result['extra']['dataflow_trace']['intermediate_vars']
-                no_of_vars = len(vars)
-                print("No. of intermediate vars:", no_of_vars)
-            except:
-                print("NO INTERMEDIATE VARS")
-                no_of_vars = 0
-            
-            if no_of_vars > 1:
-                tainted_lines = {}
-                line_numbers_ordered = []
+            inter_vars = dataflow_trace.get("intermediate_vars")
+            vars_len = 0
+            if inter_vars:
+                vars_len = len(inter_vars)
+                print("No. of intermediate vars:", vars_len)
 
-                for var in vars:
+                if vars_len > 1:
+                    tainted_lines = {}
+                    line_numbers_ordered = []
 
-                    # ignore first intermediate_vars obj
-                    if vars.index(var) == 0:
-                        print('ignoring')
+                    for var in inter_vars:
+                        # ignore first intermediate_vars obj
+                        if inter_vars.index(var) == 0:
+                            print("ignoring")
 
-                    else:
+                        else:
+                            # get tainted line numbers
+                            line_no = var["location"]["start"]["line"]
+                            line_numbers_ordered.append(line_no)
+                            print(line_no)
+                            # get tainted lines
+                            file = open(f"SHARED/EXTRACTED/{vuln_file}")
+                            for i, line in enumerate(file):
+                                if i == line_no - 1:
+                                    tainted_lines[line_no] = line.strip()
 
-                        # get tainted line numbers
-                        line_no = var['location']['start']['line']
-                        line_numbers_ordered.append(line_no)
-                        print(line_no)
-                        # get tainted lines
-                        file = open(f'SHARED/EXTRACTED/{vuln_file}')
-                        for i, line in enumerate(file):
-                            if i == line_no-1:
-                                tainted_lines[line_no] = line.strip()
+                    print("Tainted Lines: ", tainted_lines)
+                    print("Line numbers ordered: ", line_numbers_ordered)
+                    more_details = """
+    <br>
+    <h5><u>More details on tainted variables</u></h5>
+    <pre class="code-block">"""
 
-                print("Tainted Lines: ", tainted_lines)
-                print("Line numbers ordered: ", line_numbers_ordered)
-                more_details = '''
-<br>
-<h5><u>More details on tainted variables</u></h5>
-<pre class="code-block">'''
+                    for i, line in enumerate(tainted_lines):
+                        more_details += f"""<code class="code-taint">
+        {line}&#9;&#9;<mark class="mark-taint" id="code-taint-{result_no}">{tainted_lines[line]}</mark>&#9;<span class="code-comment">/* Tainted */</span></code>"""
+                        if len(line_numbers_ordered) - i != 1:
+                            line_diff = abs(
+                                line_numbers_ordered[i] - line_numbers_ordered[i + 1]
+                            )
+                            print(line_diff)
 
-                counter = 0
-                for line in tainted_lines:
-                    more_details += f'''<code class="code-taint">
-    {line}&#9;&#9;<mark class="mark-taint" id="code-taint-{result_no}">{tainted_lines[line]}</mark>&#9;<span class="code-comment">/* Tainted */</span></code>'''
-                    if len(line_numbers_ordered) - counter != 1:
-                        line_diff = abs(line_numbers_ordered[counter] - line_numbers_ordered[counter+1])
-                        print(line_diff)
+                            if line_diff > 1:
+                                more_details += """
+        <code>...&#9;&#9;...</code>"""
 
+                    more_details += """
+                    </pre>"""
+                    code_segment += more_details
 
-                        if line_diff > 1:
-                            more_details += '''
-    <code>...&#9;&#9;...</code>'''
-                            
-                    
-                            
-                    counter += 1
+                    # check for line diff between current tainted line and prev line (tainted OR source line)
 
-                    
-                
-                more_details += '''
-                </pre>'''
-                code_segment += more_details
+                    # if line diff == 1:
+                    #     dont add ...
+                    # else:
+                    #     add ...
+                    # add tainted line
 
-                #check for line diff between current tainted line and prev line (tainted OR source line)
+                    # check for line diff between sink and last tainted line (need add ...?)
 
-
-
-
-                # if line diff == 1:
-                #     dont add ...
-                # else:
-                #     add ...
-                # add tainted line
-
-                #check for line diff between sink and last tainted line (need add ...?)
-
-
-                        
-
-                
-
-            add = f'''
+            add = f"""
 <!-- Source-Sink pair -->
         <div class="card static-result">
             <div class="card-header">
@@ -333,30 +299,23 @@ def static_analysis(extension: Path, soup):
 
             </div>
         </div>
-'''
-            
+"""
+
             add_parsed = BeautifulSoup(add, "html.parser")
-            html_static_main = soup.find(id='static-main')
-            html_static_main.append(add_parsed)
+            soup.find(id="static-main").append(add_parsed)
 
             result_no += 1
 
-
-            
-
-
-
     # Initialise report name
-    report_name = f'{extension.name} ({scan_start}).html'
+    report_path = Path(
+        f"SHARED/REPORTS/{extension.name} ({scan_start.replace(':','-')}).html"
+    )
 
-    with open(f"SHARED/REPORTS/{report_name}", "w") as file:
+    with open(report_path, "w") as file:
         file.write(str(soup))
 
 
-
-
-
-def dynamic_analysis(extracted: Path):
+def dynamic_analysis(extension: Path):
     dynamic()
     pass
 
@@ -364,17 +323,8 @@ def dynamic_analysis(extracted: Path):
 if __name__ == "__main__":
     print("Start of program")
 
-
-
-    
     for extension in extraction():
-
-
-
-
-
-
-        with open('SHARED/REPORTS/report_template.html', 'r') as f:
+        with open("SHARED/REPORTS/report_template.html", "r") as f:
             html_content = f.read()
 
         # Parse HTML content
@@ -384,11 +334,12 @@ if __name__ == "__main__":
         scan_start = formatdate(localtime=True)
 
         # Update scan date in report
-        html_scan_date = soup.find(id="scan-date")
-        html_scan_date.string = scan_start
+        # html_scan_date = soup.find(id="scan-date")
+        # assert type(html_scan_date) == Tag
+        # html_scan_date.string = scan_start
+        soup.find(id="scan-date").string = scan_start
 
         # Start static analysis
-        static_analysis(extension,soup)
-
+        static_analysis(extension, soup)
 
         # dynamic_anaylsis(extension)
