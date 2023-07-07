bca32c3f91ddb929c8ea38eae1dccea3fec2713e
gcmaximus <jeraldyeo10@gmail.com>
2023-07-07 16:14:59 +0800

    dk

--- SHARED/EXTRACTED/window.addEvent.Listerner.message_new/window.addEvent.Listerner.message_new/background.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("Message received in background script:", request);
-    sendResponse("Message received!");
-});
