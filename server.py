4e4bf14c8c1d683e9611ce92cbee3780c58c45d5
Dei-Filius <hwdkong@hotmail.sg>
2023-07-12 12:21:29 +0800

    updated server

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI, Request
+
 # from fastapi import FastAPI, Form
 # from typing import Annotated
 import uvicorn
+
 # from pydantic import BaseModel
 
 # class InputData(BaseModel):
 #     data: str
 
+
 async def extract_data(req: Request):
     obj = {}
-    keys = ["method", "url", "headers", "path_params", "query_params", "client", "cookies"]
+    keys = [
+        "method",
+        "url",
+        "headers",
+        "path_params",
+        "query_params",
+        "client",
+        "cookies",
+    ]
     for key in keys:
         obj[key] = getattr(req, key)
     if obj["method"] == "POST" and obj["url"] == "http://127.0.0.1:8000/xss":
@@ -17,48 +28,45 @@ async def extract_data(req: Request):
             obj["form"] = form
     return obj
 
+
 def main():
-    data = []
+    data: dict[int, list[dict]] = {}
 
     app = FastAPI()
 
-    @app.get("/xss")
-    async def get_xss(req: Request):
+    @app.get("/xss/{pid}")
+    async def get_xss(pid: int, req: Request):
         nonlocal data
-        data.append(await extract_data(req))
+        arr: list = data.setdefault(pid, [])
+        arr.append(await extract_data(req))
         return {"message": 200}
 
-    
-    @app.post("/xss")
-    async def post_xss(req: Request):
+    @app.post("/xss/{pid}")
+    async def post_xss(pid: int, req: Request):
         nonlocal data
-        data.append(await extract_data(req))
-        # Process the data as needed
+        arr: list = data.setdefault(pid, [])
+        arr.append(await extract_data(req))
         return {"message": 200}
 
-    
-    @app.get("/data")
-    async def get_data():
+    @app.get("/data/{pid}")
+    async def get_data(pid: int):
         nonlocal data
-        return {"data": data}
-    
-    
+        return {"data": data.get(pid, [])}
+
     @app.delete("/data")
     async def del_data():
         nonlocal data
-        data = []
-        # for i in alldata:
-        #     alldata.pop(i)
+        data = {}
         return {"message": "deleted"}
 
+    uvicorn.run(app=app, host="127.0.0.1", port=8000, log_level="critical")
 
-    uvicorn.run(app=app, host="127.0.0.1", port=8000, log_level='critical')
 
 if __name__ == "__main__":
     main()
 
 # Payload we might be using:
-# 
+#
 # <form action="http://127.0.0.1:8000/xss" method="post">
 #     <label for="data">Data:</label>
 #     <input type="text" id="data" name="data">
