b90d310c42b7a31f6424f8e61b87c4786c8735fc
gcmaximus <jeraldyeo10@gmail.com>
2023-07-13 20:22:14 +0800

    idk

---
+++ SHARED/EXTRACTED/long/long.js
@@ -0,0 +1,12 @@
+console.log('start of program')
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaa = window.name
+B = aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+console.log('lol')
+y = location.hash
+
+// display window name
+x = document.getElementById('hi')
+x.innerHTML = B
+
+//display location hash
+document.querySelector('#bye').outerHTML = y
