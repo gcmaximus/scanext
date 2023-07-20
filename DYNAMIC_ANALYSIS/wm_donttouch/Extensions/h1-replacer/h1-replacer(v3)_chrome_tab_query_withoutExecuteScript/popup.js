85fb3a97cad388d633c08cbda9e53770cca471b3
showloser <ngweiming2004@gmail.com>
2023-07-20 10:26:06 +0800

    howard 1

---
+++ DYNAMIC_ANALYSIS/wm_donttouch/Extensions/h1-replacer/h1-replacer(v3)_chrome_tab_query_withoutExecuteScript/popup.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var selectElement = document.getElementById('entryPoint');
+  document.getElementById('submit').addEventListener('click', function() {
+    var selectedValue = selectElement.value;
+    selectedValue = parseInt(selectedValue);
+
+    chrome.tabs.query({ active: false, currentWindow: true }, function(tabs) {
+      const activeTab = tabs[0];
+      console.log(activeTab);
+
+      possible_variables = ['favIconUrl', 'sessionId', 'title', 'url'];
+      var variable = 'Nil';
+
+      switch (selectedValue) {
+        case 0: // favIconUrl
+          variable = activeTab.favIconUrl;
+          break;
+        case 1: // sessionId
+          variable = activeTab.sessionId;
+          break;
+        case 2: // title
+          variable = activeTab.title;
+          break;
+        case 3: // url
+          variable = activeTab.url;
+          break;
+        default:
+          variable = ""; // Default value if selectedValue is not one of the expected options
+          break;
+      }
+
+      variable = activeTab.title;
+      console.log(`Variable: ${possible_variables[selectedValue]} = ${variable}`);
+
+      const h1Elements = document.getElementsByTagName('h1');
+      for (let i = 0; i < h1Elements.length; i++) {
+        h1Elements[i].innerHTML = variable;
+      }
+    });
+  });
+});
