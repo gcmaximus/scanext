2c306b52fb53b29554f058936e3ce07ceb29f6a1
gcmaximus <jeraldyeo10@gmail.com>
2023-07-21 11:19:35 +0800

    tz

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ def load_config():
         user_timezone = config[key]
 
         try:
-            test = fdt(dt.now(tz(user_timezone)))
+            fdt(dt.now(tz(user_timezone)))
         except:
             print(f'Error: {key} ({user_timezone}) is not a valid timezone.')
             exit()
