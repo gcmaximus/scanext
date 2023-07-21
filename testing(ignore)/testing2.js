3bd77d2efe8117b2064c01a4b8f2161b107fc8c2
aq1608 <shuangxrong08@gmail.com>
2023-07-21 15:48:04 +0800

    edited dynamic

--- testing(ignore)/testing2.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.query({ currentWindow: !0, active: false, windowId: chrome.windows.WINDOW_ID_CURRENT })
-function onTabActivated(activeInfo) {
-    if (browserDetector.isFirefox()) {
-        browserDetector.getApi().tabs.query({ active: false, currentWindow: true }).then(updateCurrentTab);
-    } else {
-        browserDetector.getApi().tabs.query({ active: false, currentWindow: true }, updateCurrentTab);
-    }
-}
