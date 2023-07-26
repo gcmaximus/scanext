4b0d107300a56bc0a872ad88ce53f6602ee273f8
Dei-Filius <63829092+Dei-Filius@users.noreply.github.com>
2023-07-27 03:26:29 +0800

    Update setup_win.bat

--- setup_win.bat
+++ setup_win.bat
@@ -1,4 +1,37 @@
 @echo off
+
+
+set usage =^
+"
+Usage:
+    ./setup_lnx.sh [OPTIONS] DIR
+
+Args:
+    DIR     The absolute path to a directory used for docker volume.
+            This directory is analogous to a shared folder.
+            Refer to the user guide for more info on this directory.
+Options:
+    -h      Display usage info.
+"
+
+set finish =^
+"
+Done! Docker container scan_cont started. Run this command to start main.py:
+    docker exec -it scanext_cont python3 main.py
+
+Use this command to stop the container:
+    docker container stop scan_cont
+"
+
+if $1 == "-h"
+then
+    echo $usage
+    exit 0
+elif ! test -d $1
+then
+    echo "$1 not found!"
+    exit 1
+
 (docker build -t scanext --build-arg UID=1000 --build-arg GID=1000 . && ^
 docker run -dit --name scanext_cont -v %cd%\SHARED:/scanext/SHARED scanext) && (
     echo.
